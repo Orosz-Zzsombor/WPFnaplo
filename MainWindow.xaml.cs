a66b02356011ed5ab58296a4da94e4934049a9e0
Orosz-Zzsombor <101347624+Orosz-Zzsombor@users.noreply.github.com>
2023-03-16 18:36:33 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfOsztalyzas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string fajlNev = "naplo.txt";
+        //Így minden metódus fogja tudni használni.
+        List<Osztalyzat> jegyek = new List<Osztalyzat>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // todo Fájlok kitallózásával tegye lehetővé a naplófájl kiválasztását!
+            // Ha nem választ ki semmit, akkor "naplo.csv" legyen az állomány neve. A későbbiekben ebbe fog rögzíteni a program.
+            var tallozas = new Microsoft.Win32.OpenFileDialog();
+            bool? eredmeny = tallozas.ShowDialog();
+            if (eredmeny.HasValue)
+            {
+                fajlNev = tallozas.FileName;
+            }
+            else
+            {
+                fajlNev = "naplo.txt";
+            }
+            
+            // todo A kiválasztott naplót egyből töltse be és a tartalmát jelenítse meg a datagrid-ben!
+    
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+            //todo Ne lehessen rögzíteni, ha a következők valamelyike nem teljesül!
+            // a) - A név legalább két szóból álljon és szavanként minimum 3 karakterből!
+            //      Szó = A szöközökkel határolt karaktersorozat.
+            // b) - A beírt dátum újabb, mint a mai dátum
+
+            //todo A rögzítés mindig az aktuálisan megnyitott naplófájlba történjen!
+            string[] nevek = txtNev.Text.Split(" ");
+            if (nevek.Length < 2)
+            {
+                MessageBox.Show("A név legalább két szóból álljon!");
+            }
+            foreach (string nev in nevek) 
+            {
+                if (nev.Length < 3)
+                { 
+                    MessageBox.Show("A név minimum 3 karakterből álljon!");
+                }
+            }
+            DateTime mostaniIdo = DateTime.Now;
+            if (DateTime.Parse(datDatum.Text)>mostaniIdo)
+            {
+                MessageBox.Show("Ez a jövőben van!");
+            }
+            //A CSV szerkezetű fájlba kerülő sor előállítása
+            string csvSor = $"{txtNev.Text};{datDatum.Text};{cboTantargy.Text};{sliJegy.Value}";
+            //Megnyitás hozzáfűzéses írása (APPEND)
+            StreamWriter sw = new StreamWriter(fajlNev, append: true);
+            sw.WriteLine(csvSor);
+            sw.Close();
+            //todo Az újonnan felvitt jegy is jelenjen meg a datagrid-ben!
+            dgJegyek.Items.Add(sliJegy.Value);
+        }
+
+        private void btnBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            jegyek.Clear();  //A lista előző tartalmát töröljük
+            StreamReader sr = new StreamReader(fajlNev); //olvasásra nyitja az állományt
+            while (!sr.EndOfStream) //amíg nem ér a fájl végére
+            {
+                string[] mezok = sr.ReadLine().Split(";"); //A beolvasott sort feltördeli mezőkre
+                //A mezők értékeit felhasználva létrehoz egy objektumot
+                Osztalyzat ujJegy = new Osztalyzat(mezok[0], mezok[1], mezok[2], int.Parse(mezok[3])); 
+                jegyek.Add(ujJegy); //Az objektumot a lista végére helyezi
+            }
+            sr.Close(); //állomány lezárása
+
+            //A Datagrid adatforrása a jegyek nevű lista lesz.
+            //A lista objektumokat tartalmaz. Az objektumok lesznek a rács sorai.
+            //Az objektum nyilvános tulajdonságai kerülnek be az oszlopokba.
+
+
+            
+        }
+
+        private void sliJegy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblJegy.Content = sliJegy.Value; //Több alternatíva van e helyett! Legjobb a Data Binding!
+ 
+        }
+
+        //todo Felület bővítése: Az XAML átszerkesztésével biztosítsa, hogy láthatóak legyenek a következők!
+        // - A naplófájl neve
+        // - A naplóban lévő jegyek száma
+        // - Az átlag
+
+
+        //dgJegyek.ItemsSource = jegyek;
+        //naplofajlNeve.Content = fajlNev;
+        //naploJegyek.Content = jegyek.Count;
+        
+        //todo Új elemek frissítése: Figyeljen rá, ha új jegyet rögzít, akkor frissítse a jegyek számát és az átlagot is!
+
+        //todo Helyezzen el alkalmas helyre 2 rádiónyomógombot!
+        //Feliratok: [■] Vezetéknév->Keresztnév [O] Keresztnév->Vezetéknév
+        //A táblázatban a név azserint szerepeljen, amit a rádiónyomógomb mutat!
+        //A feladat megoldásához használja fel a ForditottNev metódust!
+        //Módosíthatja az osztályban a Nev property hozzáférhetőségét!
+        //Megjegyzés: Felételezzük, hogy csak 2 tagú nevek vannak
+    }
+}
+
